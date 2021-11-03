d72c1bce96186f2d0bb49a019cce6d7508dc22f9
Judit Ferenczik <jferenczik@gmail.com>
2021-11-03 08:26:48 +0100

    még nincs ellenőrízve

---
+++ boltok/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace boltok
+{
+    class Termekek
+    {
+        private string nev;
+        private int ar;
+        private int boltsorszam;
+        public Termekek(string nev, int ar, int boltsorszam) {
+            this.nev = nev;
+            this.ar = ar;
+            this.boltsorszam = boltsorszam;
+        }
+
+
+        public string getNev()
+        {
+            return this.nev;
+        }
+
+
+        public void setNev(string nev)
+        {
+           this.nev = nev; 
+        }
+
+
+        public int getAr()
+        {
+            return this.ar;
+        }
+
+
+        public void setAr(int ar)
+        {
+            this.ar = ar;
+        }
+
+
+
+        public int getBoltsorszam()
+        {
+            return this.boltsorszam;
+        }
+
+
+        public void setBoltsorszam(int bolt)
+        {
+            this.boltsorszam = bolt;
+        }
+
+    }
+
+
+   
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+        
---
+++ boltok/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace boltok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
